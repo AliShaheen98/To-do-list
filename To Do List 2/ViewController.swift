a15d3e25c68dd215d7490948c43a0ac45bfe1f17
Ali <as598102@icloud.com>
2020-11-20 20:23:41 +0200

    Initial Commit

---
+++ To Do List 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  To Do List 2
+//
+//  Created by Ali  on 20/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
